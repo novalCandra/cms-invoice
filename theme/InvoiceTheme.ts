6fa6f7cb175076c320547826c3239aa496f460a8
novalCandra <dinatanovel5@gmail.com>
2026-05-18 20:28:17 +0700

    feat : ✅ Theme Invoice

---
+++ theme/InvoiceTheme.ts
@@ -0,0 +1,41 @@
+import { typeInvoiceTheme } from "../types/type";
+
+const themeInvoice: typeInvoiceTheme[] = [
+    {
+        id: "monochrome",
+        label: "Monochrome",
+        bg: "#ffffff",
+        text: "#000000",
+        accent: "#000000",
+    },
+    {
+        id: "yellow",
+        label: "Yellow Accent",
+        bg: "#ffffff",
+        text: "#000000",
+        accent: "#ffdd00",
+    },
+    {
+        id: "red",
+        label: "Red Accent",
+        bg: "#ffffff",
+        text: "#000000",
+        accent: "#ff3333",
+    },
+    {
+        id: "blue",
+        label: "Blue Accent",
+        bg: "#ffffff",
+        text: "#000000",
+        accent: "#0066ff",
+    },
+    {
+        id: "dark",
+        label: "Dark Mode",
+        bg: "#1a1a1a",
+        text: "#ffffff",
+        accent: "#ffdd00",
+    },
+]
+
+export default themeInvoice;
